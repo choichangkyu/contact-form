ad527feebc682e3a0b958b3afe55c48b986fa124
choichangkyu <cck981007@gmail.com>
2018-03-10 19:11:30 +0900

    contact-form complete

---
+++ src/components/ContactCreate.js
@@ -0,0 +1,84 @@
+import React from 'react';
+
+export default class ContactCreate extends React.Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      name: '',
+      phone: ''
+    };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleClick = this.handleClick.bind(this);
+    this.handleKeyPress = this.handleKeyPress.bind(this);
+  }
+
+  handleChange(e){
+    let nextState = {};
+    nextState[e.target.name] = e.target.value;
+
+    this.setState(nextState);
+  }
+
+  handleClick(){
+
+    if(!this.state.name || !this.state.phone){
+      alert('입력하세요');
+      return
+    }
+    const contact = {
+      name: this.state.name,
+      phone: this.state.phone
+    };
+
+    this.props.onCreate(contact);
+
+    this.setState({
+      name: '',
+      phone: '',
+    });
+
+    this.nameInput.focus();
+  }
+
+  handleKeyPress(e){
+    if(e.charCode === 13){
+      this.handleClick();
+    }
+  }
+
+  render(){
+    return (
+      <div>
+        <h2>Create Contact</h2>
+        <p>
+          <input
+          type="text" name="name"
+          placeholder="Name"
+          value={this.state.name}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+          ref={(ref) => {this.nameInput = ref } }
+          />
+          <input
+          type="text" name="phone"
+          placeholder="Phone"
+          value={this.state.phone}
+          onChange={this.handleChange}
+          onKeyPress={this.handleKeyPress}
+          />
+        </p>
+        <button onClick={this.handleClick}>Create</button>
+      </div>
+
+    );
+  }
+}
+
+ContactCreate.propTypes = {
+  onCreate: React.PropTypes.func
+};
+
+ContactCreate.defaultProps = {
+  onCreate: () => { console.error('onCreate Error'); }
+}
